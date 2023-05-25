f0410cae4f0523d20ee031fc1c17418f9264cdf3
pepobusdbsc <133014471+pepobusdbsc@users.noreply.github.com>
2023-05-25 01:21:52 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+$(document).ready(function() {
+    // Check if Web3 has been injected by the browser (MetaMask)
+    if (typeof web3 !== 'undefined') {
+      web3 = new Web3(web3.currentProvider);
+    } else {
+      // Set the QuickNode provider URL
+      const providerUrl = 'https://delicate-prettiest-arm.bsc.discover.quiknode.pro/afbbe553aaba290fa7701612b198144d23376c8f/';
+      const provider = new Web3.providers.HttpProvider(providerUrl);
+      web3 = new Web3(provider);
+    }
+  
+    // Contract address and ABI
+    const contractAddress = '0xB4172656E6C8AEFFC45Af7BC30eEaE017BC90201'; // Replace with your contract address
+    const contractABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"token","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"},{"indexed":false,"internalType":"uint256","name":"id","type":"uint256"}],"name":"Lock","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":false,"internalType":"address","name":"withdrawer","type":"address"},{"indexed":false,"internalType":"uint256","name":"amount","type":"uint256"}],"name":"Withdraw","type":"event"},{"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"uint256","name":"","type":"uint256"}],"name":"depositsByWithdrawer","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"depositsCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_id","type":"uint256"},{"internalType":"uint256","name":"_duration","type":"uint256"}],"name":"extendLock","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_token","type":"address"}],"name":"getDepositsByTokenAddress","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_withdrawer","type":"address"}],"name":"getDepositsByWithdrawer","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getSelfAddress","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_token","type":"address"}],"name":"getTokenTotalLockedBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"tokenTotalLockedBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"","type":"address"}],"name":"withdrawableBalance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_token","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"},{"internalType":"uint256","name":"_unlockTime","type":"uint256"}],"name":"withdrawTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_token","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"},{"internalType":"uint256","name":"_unlockTime","type":"uint256"}],"name":"lockTokens","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}];
+  
+    // Contract instance
+    const contract = new web3.eth.Contract(contractABI, contractAddress);
+  
+    // Function to show the form and hide the connect button
+    function showForm() {
+      $('#connectButton').hide();
+      $('#form').show();
+    }
+  
+    // Event listener for the connect button
+    $('#connectButton').click(async function() {
+      // Check if the user has a web3 provider (e.g., MetaMask) installed
+      if (window.ethereum) {
+        try {
+          // Request account access from the user
+          await window.ethereum.request({ method: 'eth_requestAccounts' });
+          showForm();
+        } catch (error) {
+          console.log('User denied account access');
+        }
+      } else {
+        console.log('No web3 provider detected');
+      }
+    });
+  
+    // Event listener for the form submission
+    $('#form').submit(async function(event) {
+      event.preventDefault();
+  
+      // Get form values
+      const tokenAddress = $('#tokenAddress').val();
+      const amount = $('#amount').val();
+      const withdrawer = $('#withdrawer').val();
+      const unlockDateTime = $('#unlockDateTime').val();
+  
+      // Call the lockTokens function
+      lockTokens(tokenAddress, amount, withdrawer, unlockDateTime);
+    });
+  
+    // Function to lock tokens
+    async function lockTokens(tokenAddress, amount, withdrawer, unlockDateTime) {
+      // Convert the unlock date and time to UNIX timestamp
+      const unlockTimestamp = Math.floor(new Date(unlockDateTime).getTime() / 1000);
+  
+      // Lock tokens by calling the contract's lockTokens function
+      await contract.methods.lockTokens(tokenAddress, amount, unlockTimestamp).send({ from: withdrawer })
+        .on('transactionHash', function(hash) {
+          // Transaction sent, show loading indicator or message
+          console.log('Transaction sent');
+        })
+        .on('receipt', function(receipt) {
+          // Transaction receipt received, show success message
+          console.log('Transaction successful');
+          $('#result').text('Tokens locked successfully!');
+        })
+        .on('error', function(error) {
+          // Error occurred, show error message
+          console.error(error);
+          $('#result').text('Error locking tokens');
+        });
+    }
+  });
+  
